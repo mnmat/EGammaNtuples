466dd8d75b7c5e8fa3ef65ead8bcdfbf3cd3834e
Mark <m.matthewman@gmail.com>
2024-02-28 20:41:09 +0100

    Write features to root file

--- python/Config.py
+++ python/Config.py
@@ -13,7 +13,12 @@ process.source = cms.Source("PoolSource",
             'file:/afs/cern.ch/work/m/mmatthew/private/egamma/CMSSW_13_1_0/src/tutorialDir/DoubleElectron_FlatPt-1To100-gun/hlt_IDEAL.root'
             #'file:/afs/cern.ch/work/m/mmatthew/private/deleteMe/CMSSW_13_1_0/src/Phase2_HLT.root'    
                 )
-                            )
+            )
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("output.root"),
+                                   closeFileFast = cms.untracked.bool(True)
+                               )
 
 process.demo = cms.EDAnalyzer('EGammaNtuples',
     genParticles = cms.untracked.InputTag('genParticles'),
