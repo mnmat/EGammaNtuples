f673a730fd4c39a4a9f9cfd13dc148ef932778aa
Mark <m.matthewman@gmail.com>
2024-08-30 10:41:37 +0200

    Add genmatching and remove HLT Shower Shapes

--- python/Config.py
+++ python/Config.py
@@ -10,12 +10,15 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
                                 fileNames = cms.untracked.vstring(
-            'file:/afs/cern.ch/work/m/mmatthew/private/egamma/CMSSW_13_1_0/src/Phase2_HLT.root'
+            'root://xrootd-cms.infn.it//store/user/mmatthew/crab_projects/egamma/DoublePhoton_FlatPt-1To100-gun/crab_photon/240722_083350/0000/Phase2_IDEAL_HLT_14.root'
             #'file:/afs/cern.ch/work/m/mmatthew/private/egamma/CMSSW_13_1_0/src/tutorialDir/DoubleElectron_FlatPt-1To100-gun/hlt_IDEAL.root'
             #'file:/afs/cern.ch/work/m/mmatthew/private/deleteMe/CMSSW_13_1_0/src/Phase2_HLT.root'    
                 )
             )
 
-process.demo = cms.EDAnalyzer('EGammaNtuples')
+
+from PlayGround.EGammaNtuples.EGammaNtuples_cfi import EGammaNtuples
+process.demo = EGammaNtuples.clone(pType = "pho")
+
 
 process.p = cms.Path(process.demo)
